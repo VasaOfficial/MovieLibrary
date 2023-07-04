1ead55bb4b2007e45e25623b26442edc71043117
unknown <VasaPofficial@gmail.com>
2023-07-04 15:00:20 +0200

    feat: add header with logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import Image from "next/image"
+import Logo from 'public/assets/logo4.png'
+
 export default function Home() {
   return (
-    <h2>Hello World</h2>
+    <div className="bg-black">
+      <div className="mx-auto flex min-h-screen max-w-5xl flex-col px-4">
+        <header className="flex items-center justify-between py-8">
+          <Image
+          src={Logo}
+          width={250}
+          height={250}
+          alt="Picture of the author"
+          priority={true}
+          />
+        </header>
+        <main className="flex-1">
+          <section>
+            <div className="relative z-0 mx-auto max-w-3x1 pb-24 pt-12 text-center"></div>
+          </section>
+        </main>
+      </div>
+    </div>
   )
 }
