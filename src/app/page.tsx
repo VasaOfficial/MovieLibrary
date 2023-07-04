ed4e34b96b3a8d1a0aaae6e99779831616656ba3
unknown <VasaPofficial@gmail.com>
2023-07-04 15:54:39 +0200

    feat: add selector for user to choose options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Logo from 'public/assets/logo4.png'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 export default function Home() {
   return (
@@ -23,6 +24,18 @@ export default function Home() {
               <div>
                 <h1 className="mb-8 bg-gradient-to-t from-[#6d6d6d] to-[#f4f4f4] bg-clip-text text-4xl text-transparent md:text-5xl">Generate show or movie recommendations with Open AI</h1>
                 <p className="inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-xl text-transparent">What kind of cinema are you searching for?</p>
+                <div className="flex justify-center mt-9">
+                  <Select>
+                    <SelectTrigger className="w-[300px] text-white">
+                      <SelectValue  placeholder="Movie" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="movie">Movie</SelectItem>
+                      <SelectItem value="tv show">Tv Show</SelectItem>
+                      <SelectItem value="tv show or movie">No Preference</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
               </div>
             </div>
           </section>
